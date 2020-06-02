37c0023444e4746ef01f0276c61d0f8954a0d4cb
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-06-02 22:24:03 +0530

    template added

--- public/app.py
+++ public/app.py
@@ -1,16 +1,15 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template
 import os
 
 app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return "Hello World"
-
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'images/icon.ico')
 
-            
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
